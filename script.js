fde85ce0751feba85a7c9f6b1ca0902e454ada2b
Angela Bassa <ange@Anges-MacBook.local>
2016-05-28 11:55:57 -0400

    Revert "Update script.js"
    This reverts commit 6a1b98dc8abaa84eb6dbff3d2d6451d1cddc0828.

--- script.js
+++ script.js
@@ -1,26 +1,70 @@
-let script = new Script({  
+'use strict';
+
+const _ = require('lodash');
+const Script = require('smooch-bot').Script;
+
+const scriptRules = require('./script.json');
+
+module.exports = new Script({
+    processing: {
+        prompt: (bot) => bot.say('Beep boop...'),
+        receive: () => 'processing'
+    },
+
     start: {
         receive: (bot) => {
-            return bot.say('Hi! I\'m AngeBot, Angela\'s personal chat bot. 🤖 Chat bots are very hot right now.')
-                .then(() => 'askName');
+            return bot.say('So you want to learn about Angela? Just say HELLO to get started.')
+                .then(() => 'speak');
         }
     },
 
-    askName: {
-        prompt: (bot) => bot.say('What\'s your name?'),
+    speak: {
         receive: (bot, message) => {
-            const name = message.text;
-            return bot.setProp('name', name)
-                .then(() => bot.say(`Great! I'll call you ${name}`))
-                .then(() => 'finish');
-        }
-    },
 
-    finish: {
-        receive: (bot, message) => {
-            return bot.getProp('name')
-                .then((name) => bot.say(`Hello again, ${name}!`))
-                .then(() => 'finish');
+            let upperText = message.text.trim().toUpperCase();
+
+            function updateSilent() {
+                switch (upperText) {
+                    case "CONNECT ME":
+                        return bot.setProp("silent", true);
+                    case "DISCONNECT":
+                        return bot.setProp("silent", false);
+                    default:
+                        return Promise.resolve();
+                }
+            }
+
+            function getSilent() {
+                return bot.getProp("silent");
+            }
+
+            function processMessage(isSilent) {
+                if (isSilent) {
+                    return Promise.resolve("speak");
+                }
+
+                if (!_.has(scriptRules, upperText)) {
+                    return bot.say(`I didn't understand that.`).then(() => 'speak');
+                }
+
+                var response = scriptRules[upperText];
+                var lines = response.split('\n');
+
+                var p = Promise.resolve();
+                _.each(lines, function(line) {
+                    line = line.trim();
+                    p = p.then(function() {
+                        console.log(line);
+                        return bot.say(line);
+                    });
+                })
+
+                return p.then(() => 'speak');
+            }
+
+            return updateSilent()
+                .then(getSilent)
+                .then(processMessage);
         }
     }
 });
